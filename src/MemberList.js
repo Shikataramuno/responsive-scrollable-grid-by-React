d394ef4f01d74ed000fe30560a316764ee34e131
ptpit001 <nomura.takashi@pitarpit.com>
2019-03-20 13:26:51 +0900

    typescript版と初期化のロジックを合わせた

--- src/MemberList.js
+++ src/MemberList.js
@@ -14,34 +14,6 @@ export class MemberList extends Component {
       sortOrders: {},
       sortKey: ""
     }
-    this.memberList = [
-      {id: 1, name: 'aaaa', admin: true, progress: 10, address: 'aaaa@shikataramuno.com'},
-      {id: 2, name: 'bbbb', admin: true, progress: 20, address: 'bbbb@shikataramuno.com'},
-      {id: 3, name: 'cccc', admin: false, progress: 30, address: 'cccc@shikataramuno.com'},
-      {id: 4, name: 'dddd', admin: false, progress: 40, address: 'dddd@shikataramuno.com'},
-      {id: 5, name: 'eeee', admin: false, progress: 50, address: 'eeee@shikataramuno.com'},
-      {id: 6, name: 'ffff', admin: false, progress: 60, address: 'ffff@shikataramuno.com'},
-      {id: 7, name: 'gggg', admin: false, progress: 70, address: 'gggg@shikataramuno.com'},
-      {id: 8, name: 'hhhh', admin: false, progress: 80, address: 'hhhh@shikataramuno.com'},
-      {id: 9, name: 'iiii', admin: false, progress: 90, address: 'iiii@shikataramuno.com'},
-      {id: 10, name: 'jjjj', admin: false, progress: 11, address: 'jjjj@shikataramuno.com'},
-      {id: 11, name: 'kkkk', admin: false, progress: 22, address: 'kkkk@shikataramuno.com'},
-      {id: 12, name: 'llll', admin: false, progress: 33, address: 'llll@shikataramuno.com'},
-      {id: 13, name: 'mmmm', admin: false, progress: 44, address: 'mmmm@shikataramuno.com'},
-      {id: 14, name: 'nnnn', admin: false, progress: 55, address: 'nnnn@shikataramuno.com'},
-      {id: 15, name: 'oooo', admin: false, progress: 66, address: 'oooo@shikataramuno.com'},
-      {id: 16, name: 'pppp', admin: false, progress: 77, address: 'pppp@shikataramuno.com'},
-      {id: 17, name: 'qqqq', admin: false, progress: 88, address: 'qqqq@shikataramuno.com'},
-      {id: 18, name: 'rrrr', admin: false, progress: 99, address: 'rrrr@shikataramuno.com'},
-      {id: 19, name: 'ssss', admin: false, progress: 10, address: 'ssss@shikataramuno.com'},
-      {id: 20, name: 'tttt', admin: false, progress: 20, address: 'tttt@shikataramuno.com'},
-      {id: 21, name: 'uuuu', admin: false, progress: 30, address: 'uuuu@shikataramuno.com'},
-      {id: 22, name: 'vvvv', admin: false, progress: 40, address: 'vvvv@shikataramuno.com'},
-      {id: 23, name: 'wwww', admin: false, progress: 50, address: 'wwww@shikataramuno.com'},
-      {id: 24, name: 'xxxx', admin: false, progress: 60, address: 'xxxx@shikataramuno.com'},
-      {id: 25, name: 'yyyy', admin: false, progress: 70, address: 'yyyy@shikataramuno.com'},
-      {id: 26, name: 'zzzz', admin: true, progress: 80, address: 'zzzz@shikataramuno.com'}
-    ];
    }
 
   filter = (e) => {
@@ -186,6 +158,34 @@ export class MemberList extends Component {
     this.state.columns.forEach((key) => {
       orders[key] = 1;
     })
+    this.memberList = [
+      {id: 1, name: 'aaaa', admin: true, progress: 10, address: 'aaaa@shikataramuno.com'},
+      {id: 2, name: 'bbbb', admin: true, progress: 20, address: 'bbbb@shikataramuno.com'},
+      {id: 3, name: 'cccc', admin: false, progress: 30, address: 'cccc@shikataramuno.com'},
+      {id: 4, name: 'dddd', admin: false, progress: 40, address: 'dddd@shikataramuno.com'},
+      {id: 5, name: 'eeee', admin: false, progress: 50, address: 'eeee@shikataramuno.com'},
+      {id: 6, name: 'ffff', admin: false, progress: 60, address: 'ffff@shikataramuno.com'},
+      {id: 7, name: 'gggg', admin: false, progress: 70, address: 'gggg@shikataramuno.com'},
+      {id: 8, name: 'hhhh', admin: false, progress: 80, address: 'hhhh@shikataramuno.com'},
+      {id: 9, name: 'iiii', admin: false, progress: 90, address: 'iiii@shikataramuno.com'},
+      {id: 10, name: 'jjjj', admin: false, progress: 11, address: 'jjjj@shikataramuno.com'},
+      {id: 11, name: 'kkkk', admin: false, progress: 22, address: 'kkkk@shikataramuno.com'},
+      {id: 12, name: 'llll', admin: false, progress: 33, address: 'llll@shikataramuno.com'},
+      {id: 13, name: 'mmmm', admin: false, progress: 44, address: 'mmmm@shikataramuno.com'},
+      {id: 14, name: 'nnnn', admin: false, progress: 55, address: 'nnnn@shikataramuno.com'},
+      {id: 15, name: 'oooo', admin: false, progress: 66, address: 'oooo@shikataramuno.com'},
+      {id: 16, name: 'pppp', admin: false, progress: 77, address: 'pppp@shikataramuno.com'},
+      {id: 17, name: 'qqqq', admin: false, progress: 88, address: 'qqqq@shikataramuno.com'},
+      {id: 18, name: 'rrrr', admin: false, progress: 99, address: 'rrrr@shikataramuno.com'},
+      {id: 19, name: 'ssss', admin: false, progress: 10, address: 'ssss@shikataramuno.com'},
+      {id: 20, name: 'tttt', admin: false, progress: 20, address: 'tttt@shikataramuno.com'},
+      {id: 21, name: 'uuuu', admin: false, progress: 30, address: 'uuuu@shikataramuno.com'},
+      {id: 22, name: 'vvvv', admin: false, progress: 40, address: 'vvvv@shikataramuno.com'},
+      {id: 23, name: 'wwww', admin: false, progress: 50, address: 'wwww@shikataramuno.com'},
+      {id: 24, name: 'xxxx', admin: false, progress: 60, address: 'xxxx@shikataramuno.com'},
+      {id: 25, name: 'yyyy', admin: false, progress: 70, address: 'yyyy@shikataramuno.com'},
+      {id: 26, name: 'zzzz', admin: true, progress: 80, address: 'zzzz@shikataramuno.com'}
+    ];
     this.setState({sortOrders: orders}); 
     this.setState({members: this.memberList});
   }
